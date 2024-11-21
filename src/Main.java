84725dacb7edead97826079c0bc3af1d21c706c2
spassimirag <spassimiragenova@gmai.com>
2024-11-21 12:35:40 +0200

    Commit 1.0

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;//TI// P To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Random randomNumber = new Random();
+        int generatedNumber = randomNumber.nextInt(100);
+        int attemptsCounter = 0;
+
+        System.out.println("Please pick a number between 0 and 100 and try to guess!");
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+
+            int userGuess = input.nextInt();
+            attemptsCounter++;
+
+            if (generatedNumber == userGuess) {
+                System.out.println(userGuess + " is the right number!");
+                System.out.println("You guessed the number after " + attemptsCounter + " attempts.");
+                break;
+
+            } else if (generatedNumber < userGuess) {
+                System.out.println("Actual number is lower than your input. Try again.");
+            } else if (generatedNumber > userGuess) {
+                System.out.println("Actual number is higher than your input. Try again.");
+            }
+        }
+
+    }
+}
