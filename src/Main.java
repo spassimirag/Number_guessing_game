9005526416a3d09a074d2ff5a8291dd385de4c0b
spassimirag <spassimiragenova@gmai.com>
2024-11-21 12:38:19 +0200

    Commit 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Random;
-import java.util.Scanner;//TI// P To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
